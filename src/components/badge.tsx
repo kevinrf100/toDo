a68880f0aa39a155cfc09f0ecdae846ac187e09b
Kevin Rossetti Fernandes <30449737+kevinrf100@users.noreply.github.com>
2025-10-22 15:16:01 -0300

    Creating badge component

---
+++ src/components/badge.tsx
@@ -0,0 +1,61 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { Text } from "./text";
+
+const badgeVariant = cva(
+  "inline-flex items-center justify-center rounded-full",
+  {
+    variants: {
+      variant: {
+        primary: "bg-green-light",
+        secondary: "bg-pink-light",
+      },
+      size: {
+        sm: "py.0.5 px-2",
+        md: "px-3 py-1.5",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  }
+);
+
+const badgeTextVariant = cva("", {
+  variants: {
+    variant: {
+      primary: "text-green-dark",
+      secondary: "text-pink-dark",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+type badgeProps = {
+  className?: string;
+  children: React.ReactNode;
+} & React.ComponentProps<"div"> &
+  VariantProps<typeof badgeVariant>;
+
+const Badge = ({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}: badgeProps) => {
+  return (
+    <div className={badgeVariant({ variant, size, className })} {...props}>
+      <Text
+        variant="body-sm-bold"
+        className={badgeTextVariant({ variant: variant ?? undefined })}
+      >
+        {children}
+      </Text>
+    </div>
+  );
+};
+
+export { Badge };
